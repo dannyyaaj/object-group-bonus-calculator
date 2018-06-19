d7295b96eb5181b2fd2583bbe4edd9bce99ed51d
Danny Yang <dannyyaaj@gmail.com>
2018-06-19 14:10:35 -0500

    completed group project challenge

--- client.js
+++ client.js
@@ -1,5 +1,5 @@
-class Employee{
-  constructor( name, employeeNumber, annualSalary, reviewRating ){
+class Employee {
+  constructor(name, employeeNumber, annualSalary, reviewRating) {
     this.name = name;
     this.employeeNumber = employeeNumber;
     this.annualSalary = annualSalary;
@@ -7,14 +7,59 @@ class Employee{
   } // end constructor
 } // end Employee class
 
-const atticus = new Employee( 'Atticus', '2405', '47000', 3 );
-const jem = new Employee( 'Jem', '62347', '63500', 4 );
-const scout =  new Employee( 'Scout', '6243', '74750', 5 );
-const robert =  new Employee( 'Robert', '26835', '66000', 1 );
-const mayella =  new Employee( 'Mayella', '89068', '35000', 2 );
+const atticus = new Employee('Atticus', '2405', '47000', 3);
+const jem = new Employee('Jem', '62347', '63500', 4);
+const scout = new Employee('Scout', '6243', '74750', 5);
+const robert = new Employee('Robert', '26835', '66000', 1);
+const mayella = new Employee('Mayella', '89068', '35000', 2);
 
-const employees = [ atticus, jem, scout, robert, mayella ];
+const employees = [atticus, jem, scout, robert, mayella];
 
 // YOU SHOULD NOT NEED TO CHANGE ANYTHING ABOVE THIS POINT
 
-console.log( employees );
+console.log(employees);
+
+for (let i = 0; i < employees.length; i++) {
+  console.log(employeeComp(employees[i]));
+}
+
+function employeeComp(employee) {
+  let bonusPercentage = bonusCalculator(employee);
+  let totalBonus = (bonusPercentage / 100) * parseInt(employee.annualSalary);
+  let totalCompensation = Math.round(totalBonus + parseInt(employee.annualSalary));
+
+  let employeeBonus = {
+    name: employee.name,
+    bonusPercentage: bonusPercentage,
+    totalCompensation: totalCompensation,
+    totalBonus: totalBonus
+  };
+  return employeeBonus;
+}
+
+function bonusCalculator(employee) {
+  let bonus = 0;
+
+  if (employee.employeeNumber.length === 4) {
+    bonus += 5;
+  }
+
+  if (parseInt(employee.annualSalary) > 65000) {
+    bonus--;
+  }
+
+  if (employee.reviewRating <= 2) {
+    bonus = 0;
+  } else if (employee.reviewRating == 3) {
+    bonus += 4;
+  } else if (employee.reviewRating == 4) {
+    bonus += 6;
+  } else if (employee.reviewRating == 5) {
+    bonus += 10;
+  }
+
+  if (bonus >= 13) {
+    bonus = 13;
+  }
+  return bonus;
+}
